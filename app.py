2565db07a834e2f5f2cc167a8348361c379ae36a
Shota Batilashvili <sh.batilashvili@rsi.ge>
2022-05-23 21:40:04 +0400

    Add index.html

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def get_main():
     return f'<h1>Hello {username}<\h1>'
 
 
+@app.route('/puppy')
+def get_puppy():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
 
